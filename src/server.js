7d966267710a3aa740df03fa629d472d21fcb2b5
junghyuntaek <jht981029@naver.com>
2021-05-25 15:56:29 +0900

    Learn middleware

--- src/server.js
+++ src/server.js
@@ -4,14 +4,30 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  };
+  console.log("Allowed, you may continue.");
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("<h1>I still love you</h1>");
+  return res.send("I love middlewares");
 };
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
 };
+
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 
 const handleListening = () =>
