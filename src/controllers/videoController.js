f88653b4b931ea3210583025a38bf0c3cd4cbf71
junghyuntaek <jht981029@naver.com>
2021-05-27 15:52:39 +0900

    Cleaning Code / Use URL Parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,13 @@
 export const trending = (req, res) => res.send("Home Page Videos");
+export const search = (req, res) => res.send("Search");
 
-export const watch = (req, res) => res.send("Watch Videos");
-export const edit = (req, res) => res.send("Edit Videos");
+export const upload = (req, res) => res.send("Upload");
+export const see = (req, res) => {
+  return res.send(`Watch Video #${req.params.id}`);
+}
+export const edit = (req, res) => {
+  return res.send("Edit");
+}
+export const remove = (req, res) => {
+  return res.send("Remove");
+}
