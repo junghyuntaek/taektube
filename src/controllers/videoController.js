568729514f4030c6e01bd7e684d96c0253980613
junghyuntaek <jht981029@naver.com>
2021-05-30 00:48:05 +0900

    Use Pug ( partials / extends / variable )

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,9 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search");
 
 export const upload = (req, res) => res.send("Upload");
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  return res.send("Edit");
-}
+export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => {
   return res.send("Remove");
 }
